c93f76cfa4f8bf321392128a35f001a4e1b8a408
Valeria D'Amante <valeriadamante@gmail.com>
2023-05-16 17:42:20 +0200

    added JES

--- jet.h
+++ jet.h
@@ -7,8 +7,58 @@ namespace correction {
 class JetCorrProvider : public CorrectionsBase<JetCorrProvider> {
 public:
      enum class UncSource : int {
-        Central = 0
+        Central = -1,
+        FlavorQCD = 0,
+        RelativeBal = 1,
+        HF = 2,
+        BBEC1 = 3,
+        EC2 = 4,
+        Absolute = 5,
+        Total = 6,
+        BBEC1_2018 = 7,
+        Absolute_2018 = 8,
+        EC2_2018 = 9,
+        HF_2018 = 10,
+        RelativeSample_2018 = 11,
+        BBEC1_2017 = 12,
+        Absolute_2017 = 13,
+        EC2_2017 = 14,
+        HF_2017 = 15,
+        RelativeSample_2017 = 16,
+        BBEC1_2016 = 17,
+        Absolute_2016 = 18,
+        EC2_2016 = 19,
+        HF_2016 = 20,
+        RelativeSample_2016 = 21
     };
+
+    static const std::string getJesName(UncSource source ){
+        static const std::map<UncSource, const std::string> JesNames{
+        {UncSource::FlavorQCD,"FlavorQCD"},
+        {UncSource::RelativeBal,"RelativeBal"},
+        {UncSource::HF,"HF"},
+        {UncSource::BBEC1,"BBEC1"},
+        {UncSource::EC2,"EC2"},
+        {UncSource::Absolute,"Absolute"},
+        {UncSource::Total,"Total"},
+        {UncSource::BBEC1_2018,"BBEC1_2018"},
+        {UncSource::Absolute_2018,"Absolute_2018"},
+        {UncSource::EC2_2018,"EC2_2018"},
+        {UncSource::HF_2018,"HF_2018"},
+        {UncSource::RelativeSample_2018,"RelativeSample_2018"},
+        {UncSource::BBEC1_2017,"BBEC1_2017"},
+        {UncSource::Absolute_2017,"Absolute_2017"},
+        {UncSource::EC2_2017,"EC2_2017"},
+        {UncSource::HF_2017,"HF_2017"},
+        {UncSource::RelativeSample_2017,"RelativeSample_2017"},
+        {UncSource::BBEC1_2016,"BBEC1_2016"},
+        {UncSource::Absolute_2016,"Absolute_2016"},
+        {UncSource::EC2_2016,"EC2_2016"},
+        {UncSource::HF_2016,"HF_2016"},
+        {UncSource::RelativeSample_2016,"RelativeSample_2016"}
+    };
+        return JesNames.at(source);
+    }
     static const std::string& getScaleStr(UncScale scale)
     {
         static const std::map<UncScale, std::string> names = {
@@ -18,13 +68,44 @@ public:
         };
         return names.at(scale);
     }
+    static const int GetScaleIdx(UncScale scale){
+        static const std::map<UncScale, int> scale_indexes = {
+            {UncScale::Up, 1},
+            {UncScale::Down,2},
+            {UncScale::Central,0},
+        };
+        return scale_indexes.at(scale);
+    }
     static bool sourceApplies(UncSource source)
     {
+         if(source == UncSource::FlavorQCD) return true;
+         if(source == UncSource::RelativeBal) return true;
+         if(source == UncSource::HF) return true;
+         if(source == UncSource::BBEC1) return true;
+         if(source == UncSource::EC2) return true;
+         if(source == UncSource::Absolute) return true;
+         if(source == UncSource::Total) return true;
+         if(source == UncSource::BBEC1_2018) return true;
+         if(source == UncSource::Absolute_2018) return true;
+         if(source == UncSource::EC2_2018) return true;
+         if(source == UncSource::HF_2018) return true;
+         if(source == UncSource::RelativeSample_2018) return true;
+         if(source == UncSource::BBEC1_2017) return true;
+         if(source == UncSource::Absolute_2017) return true;
+         if(source == UncSource::EC2_2017) return true;
+         if(source == UncSource::HF_2017) return true;
+         if(source == UncSource::RelativeSample_2017) return true;
+         if(source == UncSource::BBEC1_2016) return true;
+         if(source == UncSource::Absolute_2016) return true;
+         if(source == UncSource::EC2_2016) return true;
+         if(source == UncSource::HF_2016) return true;
+         if(source == UncSource::RelativeSample_2016) return true;
         return false;
     }
-    JetCorrProvider(const std::string& ptResolution,const std::string& ptResolutionSF)
+    JetCorrProvider(const std::string& ptResolution,const std::string& ptResolutionSF, const std::string& JesTxtFile)
     {
         jetVarCalc.setSmearing(ptResolution, ptResolutionSF, false, true, 0.2, 3);
+        JesTxtFile_= JesTxtFile;
     }
 
     RVecLV getSmearing(const RVecF& Jet_pt, const RVecF& Jet_eta, const RVecF& Jet_phi,
@@ -42,8 +123,9 @@ public:
                                     GenJet_pt, GenJet_eta, GenJet_phi, GenJet_mass, event);
         RVecLV shifted_p4(Jet_pt.size());
         for (int jet_idx= 0 ; jet_idx < Jet_pt.size(); ++jet_idx){
-            shifted_p4[jet_idx] = LorentzVectorM(result.pt(static_cast<int>(source))[jet_idx], Jet_eta[jet_idx],
-            Jet_phi[jet_idx], result.mass(static_cast<int>(source))[jet_idx]);
+            int scale_idx = GetScaleIdx(scale);
+            shifted_p4[jet_idx] = LorentzVectorM(result.pt(scale_idx)[jet_idx], Jet_eta[jet_idx],
+            Jet_phi[jet_idx], result.mass(scale_idx)[jet_idx]);
         }
         return shifted_p4;
     }
@@ -51,7 +133,33 @@ public:
         return jetVarCalc.getResolution(Jet_pt, Jet_eta, rho);
     }
 
+    RVecLV getJesJet(const RVecF& Jet_pt, const RVecF& Jet_eta, const RVecF& Jet_phi,
+                    const RVecF& Jet_mass, const RVecF& Jet_rawFactor, const RVecF& Jet_area,
+                    const RVecI& Jet_jetId, const float rho, const RVecI& Jet_partonFlavour,
+                    std::uint32_t seed, const RVecF& GenJet_pt, const RVecF& GenJet_eta,
+                    const RVecF& GenJet_phi, const RVecF& GenJet_mass, int event,
+                    UncSource source, UncScale scale){
+        std::unique_ptr<JetVariationsCalculator> jvc ;
+        const UncScale jet_scale = sourceApplies(source)
+                                           ? scale : UncScale::Central;
+        const std::string& scale_str = getScaleStr(jet_scale);
+        JetCorrectorParameters jetParams(JesTxtFile_,getJesName(source));
+        jvc->addJESUncertainty(getJesName(source), jetParams);
+        auto result = jvc->produce(Jet_pt, Jet_eta, Jet_phi, Jet_mass, Jet_rawFactor,
+                                    Jet_area, Jet_jetId, rho, Jet_partonFlavour, seed,
+                                    GenJet_pt, GenJet_eta, GenJet_phi, GenJet_mass, event);
+        RVecLV shifted_p4(Jet_pt.size());
+        for (int jet_idx= 0 ; jet_idx < Jet_pt.size(); ++jet_idx){
+            int scale_idx = GetScaleIdx(scale);
+            shifted_p4[jet_idx] = LorentzVectorM(result.pt(scale_idx)[jet_idx], Jet_eta[jet_idx],
+            Jet_phi[jet_idx], result.mass(scale_idx)[jet_idx]);
+        }
+        return shifted_p4;
+    }
+
 private:
+
     JetVariationsCalculator jetVarCalc ;
+    std::string JesTxtFile_;
 };
 } // namespace correction
--- jet.py
+++ jet.py
@@ -8,13 +8,14 @@ class JetCorrProducer:
     JEC_PtRes_txtPath = "Corrections/data/JME/{}/Summer19UL18_JRV2_MC/Summer19UL18_JRV2_MC_PtResolution_AK4PFchs.txt"
     JEC_PhiRes_txtPath = "Corrections/data/JME/{}/Summer19UL18_JRV2_MC/Summer19UL18_JRV2_MC_PhiResolution_AK4PFchs.txt"
     JEC_EtaRes_txtPath = "Corrections/data/JME/{}/Summer19UL18_JRV2_MC/Summer19UL18_JRV2_MC_EtaResolution_AK4PFchs.txt"
+    JES_Regouped_txtPath = "Corrections/data/JME/{}/Summer19UL18_JRV2_MC/Regrouped_Summer19UL18_V5_MC_UncertaintySources_AK4PFchs.txt"
     initialized = False
     #Sources = []
     period = None
-
     def __init__(self, period):
         ptResolution = os.path.join(os.environ['ANALYSIS_PATH'],JetCorrProducer.JEC_PtRes_txtPath.format(period))
         ptResolutionSF = os.path.join(os.environ['ANALYSIS_PATH'],JetCorrProducer.JEC_SF_txtPath.format(period))
+        JEC_Regrouped = os.path.join(os.environ['ANALYSIS_PATH'], JetCorrProducer.JES_Regouped_txtPath.format(period))
         if not JetCorrProducer.initialized:
             ROOT.gSystem.Load("libJetMETCorrectionsModules.so")
             ROOT.gSystem.Load("libCondFormatsJetMETObjects.so")
@@ -24,7 +25,7 @@ class JetCorrProducer:
             JME_calc_path = os.path.join(headers_dir, "JMESystematicsCalculators.cc")
             ROOT.gInterpreter.Declare(f'#include "{JME_calc_path}"')
             ROOT.gInterpreter.Declare(f'#include "{header_path}"')
-            ROOT.gInterpreter.ProcessLine(f'::correction::JetCorrProvider::Initialize("{ptResolution}", "{ptResolutionSF}")')
+            ROOT.gInterpreter.ProcessLine(f'::correction::JetCorrProvider::Initialize("{ptResolution}", "{ptResolutionSF}","{JEC_Regrouped}")')
             JetCorrProducer.period = period
             JetCorrProducer.initialized = True
     def getP4Smearing(self, df, source_dict):
@@ -33,6 +34,20 @@ class JetCorrProducer:
             for scale in getScales(source):
                 syst_name = getSystName(source, scale)
                 df = df.Define(f'Jet_p4_{syst_name}', f'''::correction::JetCorrProvider::getGlobal().getSmearing(
+                                Jet_pt, Jet_eta, Jet_phi, Jet_mass, Jet_rawFactor, Jet_area,
+                                Jet_jetId, Rho_fixedGridRhoFastjetAll, Jet_partonFlavour, 0, GenJet_pt, GenJet_eta,
+                                GenJet_phi, GenJet_mass, event,
+                               ::correction::JetCorrProvider::UncSource::{source}, ::correction::UncScale::{scale})''')
+                df = df.Define(f'Jet_p4_{syst_name}_delta', f'Jet_p4_{syst_name} - Jet_p4_{nano}')
+        return df, source_dict
+
+    def getJes(self, df, source_dict):
+        for source in ["FlavorQCD","RelativeBal" "HF" "BBEC1" "EC2" "Absolute" "Total" "BBEC1_2018" "Absolute_2018" "EC2_2018" "HF_2018" "RelativeSample_2018"] :
+            updateSourceDict(source_dict, source, 'Jet')
+            for scale in getScales(source):
+                syst_name = getSystName(source, scale)
+                print(f"Jet_p4_{syst_name}")
+                df = df.Define(f'Jet_p4_{syst_name}', f'''::correction::JetCorrProvider::getGlobal().getJesJet(
                                 Jet_pt, Jet_eta, Jet_phi, Jet_mass, Jet_rawFactor, Jet_area,
                                 Jet_jetId, Rho_fixedGridRhoFastjetAll, Jet_partonFlavour, 0, GenJet_pt, GenJet_eta,
                                 GenJet_phi, GenJet_mass, event,
