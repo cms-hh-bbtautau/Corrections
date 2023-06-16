ecea23896759d27b3a2357c80930c83e21800ccb
Valeria D'Amante <valeriadamante@gmail.com>
2023-06-16 13:51:26 +0200

    bug fix

--- puJetID.py
+++ puJetID.py
@@ -22,6 +22,7 @@ class puJetIDCorrProducer:
 
     def getPUJetIDEff(self, df,isCentral=True):
         puJetID_SF_branches = []
+        df = df.Define("Jet_B0", f"v_ops::pt(Jet_p4)>20 && abs(v_ops::eta(Jet_p4)) < 5 && ( Jet_jetId & 2 ) && (Jet_puId>0 || v_ops::pt(Jet_p4)>50)")
         for source in [ central ] + puJetIDCorrProducer.puJetID_SF_Sources:
             for scale in getScales(source):
                 if not isCentral and scale!= central: continue
@@ -32,6 +33,10 @@ class puJetIDCorrProducer:
                 branch_central_jets = f"""weight_Jet_PUJetID_Central_"""
                 df = df.Define(f"{branch_name_jets}", f"""RVecF weights(Jet_p4.size(), 1); for(size_t jet_idx = 0 ; jet_idx < Jet_p4.size(); jet_idx++)
                                {{
+                                   if(!Jet_B0[jet_idx]){{
+                                       weights[jet_idx] = 1;
+                                       continue;
+                                   }}
                                     weights[jet_idx] = ::correction::PUJetIDCorrProvider::getGlobal().getPUJetID_eff(
                                         Jet_p4[jet_idx], "{puJetIDCorrProducer.puJetID}",
                                         ::correction::PUJetIDCorrProvider::UncSource::{source}, ::correction::UncScale::{scale});
