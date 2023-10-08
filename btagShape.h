d025a6e08c544621de68695d4cb12027fe5d8b31
Valeria D'Amante <valeriadamante@gmail.com>
2023-10-08 17:25:11 +0200

    added btag shape sfs

---
+++ btagShape.h
@@ -0,0 +1,150 @@
+#pragma once
+
+#include "corrections.h"
+#include "jet.h"
+
+namespace correction {
+class bTagShapeCorrProvider : public CorrectionsBase<bTagShapeCorrProvider> {
+public:
+    enum class UncSource : int {
+        Central = -1,
+        lf = 0,
+        hf = 1,
+        lfstats1 = 2,
+        lfstats2 = 3,
+        hfstats1 = 4,
+        hfstats2 = 5,
+        cferr1=6,
+        cferr2=7,
+        jesRelativeBal = 8,
+        jesHF = 9,
+        jesBBEC1 = 10,
+        jesEC2 = 11,
+        jesAbsolute = 12,
+        jesFlavorQCD = 13,
+        jesBBEC1_year = 14,
+        jesAbsolute_year = 15,
+        jesEC2_year = 16,
+        jesHF_year = 17,
+        jesRelativeSample_year = 18
+
+    };
+    static bool needYear(UncSource source){
+        if (source == UncSource::jesBBEC1_year || source==UncSource::jesAbsolute_year ||
+        source==UncSource::jesEC2_year || source == UncSource::jesHF_year ||
+        source == UncSource::jesRelativeSample_year){return true;}
+        return false;
+    }
+    static const std::map<UncSource,std::string> getUncName (){
+        static const std::map<UncSource,std::string> UncMapNames = {
+            {UncSource::Central, "Central"},
+            {UncSource::lf, "lf"},
+            {UncSource::hf, "hf"},
+            {UncSource::lfstats1, "lfstats1"},
+            {UncSource::lfstats2, "lfstats2"},
+            {UncSource::hfstats1, "hfstats1"},
+            {UncSource::hfstats2, "hfstats2"},
+            {UncSource::cferr1, "cferr1"},
+            {UncSource::cferr2, "cferr2"},
+            {UncSource::jesRelativeBal, "jesRelativeBal"},
+            {UncSource::jesHF, "jesHF"},
+            {UncSource::jesBBEC1, "jesBBEC1"},
+            {UncSource::jesEC2, "jesEC2"},
+            {UncSource::jesAbsolute, "jesAbsolute"},
+            {UncSource::jesFlavorQCD, "jesFlavorQCD"},
+            {UncSource::jesBBEC1_year, "jesBBEC1_"},
+            {UncSource::jesAbsolute_year, "jesAbsolute_"},
+            {UncSource::jesEC2_year, "jesEC2_"},
+            {UncSource::jesHF_year, "jesHF_"},
+            {UncSource::jesRelativeSample_year, "jesRelativeSample_"}
+        };
+        return UncMapNames;
+    }
+
+    static std::string getScaleStr(UncScale scale)
+    {
+        static const std::map<UncScale, std::string> scale_names = {
+            { UncScale::Down, "down_" },
+            { UncScale::Up, "up_" },
+        };
+        if(scale == UncScale::Central)
+            return "central";
+        return scale_names.at(scale) ;
+    }
+
+    static const std::string getFullNameUnc(const std::string scale_name,const std::string source_name, const std::string year, bool need_year, bool isCentral){
+        if (isCentral) return "central";
+        return need_year ? scale_name+source_name+year : scale_name+source_name;
+    }
+    //"description": "deepJet reshaping scale factors for UL 2018.
+    //The scale factors have 8 default uncertainty sources (hf,lf,hfstats1/2,lfstats1/2,cferr1/2).
+    //All except the cferr1/2 uncertainties are to be applied to light and b jets.
+    // The cferr1/2 uncertainties are to be applied to c jets.
+    //hf/lfstats1/2 uncertainties are to be decorrelated between years, the others correlated.
+    //Additional jes-varied scale factors are supplied to be applied for the jes variations.",
+    // Note the flavor convention: hadronFlavor is b = 5, c = 4, f = 0
+
+
+    static bool sourceApplies(UncSource source, int Jet_Flavour, ::correction::JetCorrProvider::UncSource jet_source)
+    {
+        if(source == UncSource::lf && (Jet_Flavour == 5 || Jet_Flavour==0) && jet_source==::correction::JetCorrProvider::UncSource::Central) return true;
+        if(source == UncSource::hf && (Jet_Flavour == 5 || Jet_Flavour==0) && jet_source==::correction::JetCorrProvider::UncSource::Central) return true;
+        if(source == UncSource::lfstats1 && (Jet_Flavour == 5 || Jet_Flavour==0) && jet_source==::correction::JetCorrProvider::UncSource::Central) return true;
+        if(source == UncSource::lfstats2 && (Jet_Flavour == 5 || Jet_Flavour==0) && jet_source==::correction::JetCorrProvider::UncSource::Central) return true;
+        if(source == UncSource::hfstats1 && (Jet_Flavour == 5 || Jet_Flavour==0) && jet_source==::correction::JetCorrProvider::UncSource::Central) return true;
+        if(source == UncSource::hfstats2 && (Jet_Flavour == 5 || Jet_Flavour==0) && jet_source==::correction::JetCorrProvider::UncSource::Central) return true;
+        if(source == UncSource::cferr1 && Jet_Flavour == 4 && jet_source==::correction::JetCorrProvider::UncSource::Central) return true;
+        if(source == UncSource::cferr2 && Jet_Flavour == 4 && jet_source==::correction::JetCorrProvider::UncSource::Central)  return true;
+        if (source==UncSource::jesRelativeBal && jet_source==::correction::JetCorrProvider::UncSource::RelativeBal ) return true;
+        if (source==UncSource::jesHF && jet_source==::correction::JetCorrProvider::UncSource::HF ) return true;
+        if (source==UncSource::jesBBEC1 && jet_source==::correction::JetCorrProvider::UncSource::BBEC1 ) return true;
+        if (source==UncSource::jesEC2 && jet_source==::correction::JetCorrProvider::UncSource::EC2 ) return true;
+        if (source==UncSource::jesAbsolute && jet_source==::correction::JetCorrProvider::UncSource::Absolute ) return true;
+        if (source==UncSource::jesFlavorQCD && jet_source==::correction::JetCorrProvider::UncSource::FlavorQCD ) return true;
+        if (source==UncSource::jesBBEC1_year && jet_source==::correction::JetCorrProvider::UncSource::BBEC1_year ) return true;
+        if (source==UncSource::jesAbsolute_year && jet_source==::correction::JetCorrProvider::UncSource::Absolute_year ) return true;
+        if (source==UncSource::jesEC2_year && jet_source==::correction::JetCorrProvider::UncSource::EC2_year ) return true;
+        if (source==UncSource::jesHF_year && jet_source==::correction::JetCorrProvider::UncSource::HF_year ) return true;
+        if (source==UncSource::jesRelativeSample_year && jet_source==::correction::JetCorrProvider::UncSource::RelativeSample_year ) return true;
+        return false;
+    }
+
+    bTagShapeCorrProvider(const std::string& fileName, const std::string& year)  :
+    corrections_(CorrectionSet::from_file(fileName)),
+    deepJet_shape_(corrections_->at("deepJet_shape")),
+    _year(year)
+    {
+    }
+
+
+    float getBTagShapeSF(const RVecLV& Jet_p4, const RVecB& pre_sel, const RVecI& Jet_Flavour,const RVecF& Jet_bTag_score, UncSource source, UncScale scale,
+    ::correction::JetCorrProvider::UncSource jet_source) const
+    {
+        for(size_t jet_idx = 0; jet_idx < Jet_p4.size(); jet_idx++){
+            if(!pre_sel[jet_idx]) continue;
+            const UncScale jet_tag_scale = sourceApplies(source, Jet_Flavour[jet_idx],jet_source)
+                                           ? scale : UncScale::Central;
+            const std::string& scale_str = getScaleStr(jet_tag_scale);
+            bool isCentral = jet_tag_scale == UncScale::Central;
+            bool need_year = needYear(source);
+            std::string source_str = getUncName().at(source);
+            const std::string& unc_name = getFullNameUnc(scale_str, source_str,_year, need_year, isCentral);
+            const auto sf = deepJet_shape_->evaluate({unc_name, Jet_Flavour[jet_idx], std::abs(Jet_p4[jet_idx].eta()),Jet_p4[jet_idx].pt(),Jet_bTag_score[jet_idx]});
+            return sf;
+        }
+
+        return 1.;
+    }
+
+private:
+
+
+private:
+    std::unique_ptr<CorrectionSet> corrections_;
+    Correction::Ref deepJet_shape_;
+    std::string _year;
+
+
+};
+
+} //namespace correction
--- jet.py
+++ jet.py
@@ -12,11 +12,15 @@ class JetCorrProducer:
     JEC_PtRes_txtPath_data = "Corrections/data/JME/{}/Summer19UL18_JRV2_DATA/Summer19UL18_JRV2_DATA_PtResolution_AK4PFchs.txt"
     JEC_PhiRes_txtPath_data = "Corrections/data/JME/{}/Summer19UL18_JRV2_DATA/Summer19UL18_JRV2_DATA_PhiResolution_AK4PFchs.txt"
     JEC_EtaRes_txtPath_data = "Corrections/data/JME/{}/Summer19UL18_JRV2_DATA/Summer19UL18_JRV2_DATA_EtaResolution_AK4PFchs.txt"
+
+    jsonPath_btag = "/cvmfs/cms.cern.ch/rsync/cms-nanoAOD/jsonpog-integration/POG/BTV/{}/btagging.json.gz"
+
     initialized = False
     #Sources = []
     period = None
     def __init__(self, period,isData):
         JetCorrProducer.isData = isData
+        jsonFile_btag = JetCorrProducer.jsonPath_btag.format(period)
         ptResolution = os.path.join(os.environ['ANALYSIS_PATH'],JetCorrProducer.JEC_PtRes_txtPath_MC.format(period))
         ptResolutionSF = os.path.join(os.environ['ANALYSIS_PATH'],JetCorrProducer.JEC_SF_txtPath_MC.format(period))
         JEC_Regrouped = os.path.join(os.environ['ANALYSIS_PATH'], JetCorrProducer.JES_Regouped_txtPath_MC.format(period))
@@ -29,12 +33,33 @@ class JetCorrProducer:
             ROOT.gSystem.Load("libCommonToolsUtils.so")
             headers_dir = os.path.dirname(os.path.abspath(__file__))
             header_path = os.path.join(headers_dir, "jet.h")
+            headershape_path = os.path.join(headers_dir, "btagShape.h")
             JME_calc_path = os.path.join(headers_dir, "JMESystematicsCalculators.cc")
             ROOT.gInterpreter.Declare(f'#include "{JME_calc_path}"')
             ROOT.gInterpreter.Declare(f'#include "{header_path}"')
+            ROOT.gInterpreter.Declare(f'#include "{headershape_path}"')
             ROOT.gInterpreter.ProcessLine(f"""::correction::JetCorrProvider::Initialize("{ptResolution}", "{ptResolutionSF}","{JEC_Regrouped}", "{period.split("_")[0]}")""")
+            ROOT.gInterpreter.ProcessLine(f"""::correction::bTagShapeCorrProvider::Initialize("{jsonFile_btag}", "{period.split("_")[0]}")""")
             JetCorrProducer.period = period
             JetCorrProducer.initialized = True
+
+    def addtbTagShapeSFInDf(df, bTagShapeSource, SF_branches, source, scale, syst_name):
+        bTagShape_branch_name = f"weight_bTagShapeSF_{syst_name}"
+        bTagShape_branch_central = f"""weight_bTagShapeSF_{getSystName(central, central)}"""
+        df = df.Define(f"{bTagShape_branch_name}_double",
+                    f''' ::correction::bTagShapeCorrProvider::getGlobal().getBTagShapeSF(
+                    Jet_p4, Jet_bCand, Jet_hadronFlavour, Jet_btagDeepFlavB,
+                    ::correction::bTagShapeCorrProvider::UncSource::{bTagShapeSource},
+                    ::correction::UncScale::{scale},
+                    ::correction::JetCorrProvider::UncSource::{source}) ''')
+        if bTagShapeSource != central:
+            df = df.Define(f"{bTagShape_branch_name}_rel", f"static_cast<float>({bTagShape_branch_name}_double/{bTagShape_branch_central})")
+            bTagShape_branch_name += '_rel'
+        else:
+            df = df.Define(f"{bTagShape_branch_name}", f"static_cast<float>({bTagShape_branch_name}_double)")
+        SF_branches.append(f"{bTagShape_branch_name}")
+        return df, SF_branches
+
 
     def getP4Variations(self, df, source_dict):
         df = df.Define(f'Jet_p4_shifted_map', f'''::correction::JetCorrProvider::getGlobal().getShiftedP4(
@@ -54,6 +79,29 @@ class JetCorrProducer:
                 df = df.Define(f'Jet_p4_{syst_name}', f'''Jet_p4_shifted_map.at({{::correction::JetCorrProvider::UncSource::{source}, ::correction::UncScale::{scale}}})''')
                 df = df.Define(f'Jet_p4_{syst_name}_delta', f'Jet_p4_{syst_name} - Jet_p4_{nano}')
         return df,source_dict
+
+    def getBtagShapeSFs(self, df, return_variations=True, isCentral=True):
+        SF_branches=[]
+        for bTagShapeSource_jesCentral in [central]+["lf", "hf", "lfstats1", "lfstats2", "hfstats1", "hfstats2", "cferr1", "cferr2"]:
+            for scale in getScales(bTagShapeSource_jesCentral):
+                bTagShapeSource_jesCentral_syst_name = getSystName(bTagShapeSource_jesCentral, scale)
+                print(bTagShapeSource_jesCentral, scale, bTagShapeSource_jesCentral_syst_name)
+                df, SF_branches= JetCorrProducer.addtbTagShapeSFInDf(df, bTagShapeSource_jesCentral, SF_branches, central, scale, bTagShapeSource_jesCentral_syst_name)
+
+        sf_sources = ["FlavorQCD","RelativeBal", "HF", "BBEC1", "EC2", "Absolute", "BBEC1_", "Absolute_", "EC2_", "HF_", "RelativeSample_" ] if return_variations else []
+        for source in sf_sources:
+            bTagShapeSource = 'jes'+source if source != central else central
+            bTahShapeSource_eff = 'jes'+source if source != central else central
+            if source.endswith("_") :
+                bTahShapeSource_eff = bTahShapeSource_eff+ JetCorrProducer.period.split("_")[0]
+                source+="year"
+                bTagShapeSource+="year"
+            for scale in getScales(source):
+                if not isCentral and scale != central:
+                    continue
+                bTagShapeSource_syst_name = getSystName(bTahShapeSource_eff, scale)
+                df, SF_branches= JetCorrProducer.addtbTagShapeSFInDf(df, bTagShapeSource, SF_branches, source, scale,bTagShapeSource_syst_name)
+        return df,SF_branches
 
     def getEnergyResolution(self, df):
         df= df.Define(f"Jet_ptRes", f""" ::correction::JetCorrProvider::getGlobal().getResolution(
